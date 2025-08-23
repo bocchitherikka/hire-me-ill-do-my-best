3598277e0df01a4df75bacded678b5919229b1c9
rikka7 <m.bondarenko.04@mail.ru>
2025-08-23 12:00:42 +0300

    Create Task model

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from enum import Enum
+from sqlalchemy import Column, Integer, String
+
+from .database import Base
+
+
+class TaskStatus(Enum):
+    created = "created"
+    in_progress = "in_progress"
+    completed = "completed"
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    status = Column(Enum(TaskStatus), default=TaskStatus.created)
